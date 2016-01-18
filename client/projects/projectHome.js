a86bac897d23b9f0c20cb62827589e38dfc3221f
jameswang14 <sambob12@gmail.com>
2016-01-17 19:54:29 -0500

    fixed template re-rendering

--- client/projects/projectHome.js
+++ client/projects/projectHome.js
@@ -2,35 +2,34 @@ if(Meteor.isClient)
 {
 	var proj;
 	var sub;
-	Tracker.autorun(function(){
-		Meteor.subscribe("projectById", Session.get("currentProjId"));
-		console.log(Projects.find().fetch());
-	});
-	// TODO: refresh subscription
+	// TODO: add private proj permissions checking
 	Template.projectHome.onCreated(function(){
-		// console.log(this.data.id);
-		// sub = Meteor.subscribe("projectById", this.data.id, function() {
-		// 	proj = Projects.find().fetch()[0];	
-		// 	console.log(proj);			
-		// });
-		proj = Projects.find({_id:this.data.id}).fetch()[0];
+		var self = this;
+		self.autorun(function(){
+			self.subscribe("projectById", Session.get("currentProjId"),
+				function()
+				{
+					proj = Projects.find().fetch()[0];
+				});
+		});
+		
 	});
-
 
 	Template.projectHome.helpers({
 		title: function() {	
-			// if(sub.ready())	
-				return proj.name;
+			return proj.name;
 		},
 		desc: function(){
-			// if(sub.ready())
-				return proj.desc;
+			return proj.desc;
 		},
 		project: function(){
-			// if(sub.ready())
-				return proj;
+			return proj;
+		},
+		isOwner: function(){
+			console.log(proj);
+			if(!Meteor.user())
+				return false;
+			return proj.owner === Meteor.user()._id;
 		}
-
-
 	});
 }
