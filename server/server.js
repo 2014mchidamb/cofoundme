94a47c794e3f0052bbb76cf8d37f530445c52d1d
jameswang14 <sambob12@gmail.com>
2016-03-26 15:19:43 -0400

    added talents search source

--- server/server.js
+++ server/server.js
@@ -1,36 +1,51 @@
 if(Meteor.isServer)
 {
-SearchSource.defineSource('projects', function(searchText, options) {
-  var options = {sort: {isoScore: -1}, limit: 20};
+  SearchSource.defineSource('talents', function(searchText, options){
+    var options = {sort: {isoScore: -1}, limit: 20};
+    if(searchText){
+      var regExp = buildRegExp(searchText);
+      var selector = {$or: [
+        {"profile.name":regExp}
+        ]};
 
-  if(searchText) {
-    var regExp = buildRegExp(searchText);
-    var selector = {$or: [
-      {name: regExp},
-      {desc: regExp},
-      {username: regExp}
-    ]};
+        return Meteor.users.find(selector, options).fetch();
+      } else
+      return Meteor.users.find({}, options).fetch();
+    });
 
-    return Projects.find(selector, options).fetch();
-  } else {
-    return Projects.find({}, options).fetch();
-  }
-});
 
-function buildRegExp(searchText) {
+  SearchSource.defineSource('projects', function(searchText, options) {
+    var options = {sort: {isoScore: -1}, limit: 20};
+
+    if(searchText) {
+      var regExp = buildRegExp(searchText);
+      var selector = {$or: [
+        {name: regExp},
+        {desc: regExp},
+        {username: regExp},
+        {ownerName: regExp}
+        ]};
+
+        return Projects.find(selector, options).fetch();
+      } else {
+        return Projects.find({}, options).fetch();
+      }
+    });
+
+  function buildRegExp(searchText) {
   // this is a dumb implementation
   var parts = searchText.trim().split(/[ \-\:]+/);
   return new RegExp("(" + parts.join('|') + ")", "ig");
 }
 
-	Accounts.onCreateUser(function(option, user)
-	{
-		if(option.profile){
-			user.profile = option.profile;
-			user.profile.owner_of = [];
-			user.profile.cofounder_of = [];
-			user.profile.member_of = [];
-		}
-        return user;
-	});
+Accounts.onCreateUser(function(option, user)
+{
+  if(option.profile){
+   user.profile = option.profile;
+   user.profile.owner_of = [];
+   user.profile.cofounder_of = [];
+   user.profile.member_of = [];
+ }
+ return user;
+});
 }
