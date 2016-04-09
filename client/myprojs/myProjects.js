f3a386187a401b14248eca62e0c398c358563503
jameswang14 <sambob12@gmail.com>
2016-04-09 00:06:11 -0400

    now uses search_template

--- client/myprojs/myProjects.js
+++ client/myprojs/myProjects.js
@@ -1,16 +1,39 @@
-if(Meteor.isClient)
-{
-	var result;
-	Tracker.autorun(function(){
-		Meteor.subscribe("projectsByUser", Meteor.userId());
-	});
-	// Template.myProjects.rendered = function() {
-	// };
+var options = {
+  keepHistory: 1000 * 60 * 5,
+  localSearch: true
+};
+var fields = ['name', 'desc', 'ownerName'];
 
-	Template.myProjects.helpers({
-		myprojs: function() {
-			var result = Projects.find().fetch();
-			return result;
-		}
-	});
-}
+Template.myProjects.onCreated(function(){
+    var self = this;
+    self.autorun(function(){
+     ProjectSearch = new SearchSource('projects', fields, options);
+  });
+});
+
+
+Template.myProjects.helpers({
+  getProjects: function() {
+    return ProjectSearch.getData({
+      transform: function(matchText, regExp) {
+        return matchText.replace(regExp, "$&");
+      },
+      sort: {isoScore: -1}
+    });
+  },
+  
+  isLoading: function() {
+    return ProjectSearch.getStatus().loading;
+  }
+});
+
+Template.myProjects.rendered = function() {
+  ProjectSearch.search('');
+};
+
+Template.searchBox.events({
+  "keyup #search-box": _.throttle(function(e) {
+    var text = $(e.target).val().trim();
+    ProjectSearch.search(text);
+  }, 200)
+});
