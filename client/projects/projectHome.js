56f1e15cb3e74f07e138d03b6b2efdca2fa12ec7
jameswang14 <sambob12@gmail.com>
2016-04-26 01:04:58 -0400

    shows project url

--- client/projects/projectHome.js
+++ client/projects/projectHome.js
@@ -30,6 +30,9 @@ if(Meteor.isClient)
 		desc: function(){
 			return proj.desc;
 		},
+		url:function(){
+			return proj.url;
+		},
         needs: function() {
             return proj.needs;
         },
