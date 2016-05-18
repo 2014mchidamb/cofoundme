c0bbdb090b892dc65a39cb3b1f6aab6d00f42403
jameswang14 <sambob12@gmail.com>
2016-05-18 03:00:27 -0400

    fixed incorrect http auto add

--- client/forms/newProj.js
+++ client/forms/newProj.js
@@ -1,10 +1,25 @@
+Template.newProj.onRendered(function(){
+    var select = document.getElementById("schools");
+    var options = ["University of Virginia"]; // TODO: load a list of schools from db
+    for(var i = 0; i < options.length; i++) {
+        var opt = options[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        select.appendChild(el);
+    }
+     $(document).ready(function() {
+        $('select').material_select();
+    });
+
+});
 Template.newProj.events({
 	"submit form": function(event) {
 		event.preventDefault();
         console.log("form submitted");
 		var projname = event.target.proj_name.value;
 		var url = event.target.proj_link.value;
-        if(url.indexOf("http") == -1)
+        if(url && url.indexOf("http") == -1)
             url = "http://" + url;
 		var school = event.target.school.value;
 		var projdesc = event.target.desc.value;
